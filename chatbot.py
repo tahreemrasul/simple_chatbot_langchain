35a0030d1c468bdb5296b080e321f14355234332
tahreem_rasul <rasul.tahreem1@gmail.com>
2024-02-03 14:23:24 +0500

    updated with memory and chainlit code

--- chatbot.py
+++ chatbot.py
@@ -1,22 +1,32 @@
 from langchain_openai import OpenAI
 from langchain.chains import LLMChain
 from prompts import ice_cream_assistant_prompt_template
+from langchain.memory.buffer import ConversationBufferMemory
 
 from dotenv import load_dotenv
 
+import chainlit as cl
+
 load_dotenv()
 
-llm = OpenAI(model='gpt-3.5-turbo-instruct',
-             temperature=0)
 
-llm_chain = LLMChain(llm=llm, prompt=ice_cream_assistant_prompt_template)
+@cl.on_chat_start
+def quey_llm():
+    llm = OpenAI(model='gpt-3.5-turbo-instruct',
+                 temperature=0)
+    conversation_memory = ConversationBufferMemory(memory_key="chat_history",
+                                                   max_len=50,
+                                                   return_messages=True,
+                                                   )
+    llm_chain = LLMChain(llm=llm, prompt=ice_cream_assistant_prompt_template, memory=conversation_memory)
+    cl.user_session.set("llm_chain", llm_chain)
 
 
-def query_llm(question):
-    print(llm_chain.invoke({'question': question})['text'])
+@cl.on_message
+async def query_llm(message: cl.Message):
+    llm_chain = cl.user_session.get("llm_chain")
+    response = await llm_chain.acall(message.content,
+                                     callbacks=[
+                                         cl.AsyncLangchainCallbackHandler()])
 
-
-if __name__ == '__main__':
-    query_llm("Who are you?")
-
-
+    await cl.Message(response["text"]).send()
