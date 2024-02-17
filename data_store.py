7f27a16b7ed2fec0ac83daf3cc7038f550009d0a
tahreem_rasul <rasul.tahreem1@gmail.com>
2024-02-17 20:18:02 +0500

    added API endpoints data

---
+++ data_store.py
@@ -0,0 +1,35 @@
+# Example menu, special offers, customer reviews, and customizations
+
+menu = {
+    "flavors": [
+        {"flavorName": "Strawberry", "count": 50},
+        {"flavorName": "Chocolate", "count": 75}
+    ],
+    "toppings": [
+        {"toppingName": "Hot Fudge", "count": 50},
+        {"toppingName": "Sprinkles", "count": 2000},
+        {"toppingName": "Whipped Cream", "count": 50}
+    ]
+}
+
+special_offers = {
+    "offers": [
+        {"offerName": "Two for Tuesday", "details": "Buy one get one free on all ice cream flavors every Tuesday."},
+        {"offerName": "Winter Wonderland Discount", "details": "25% off on all orders above $20 during the winter season."}
+    ]
+}
+
+customer_reviews = {
+    "reviews": [
+        {"userName": "andrew_1", "rating": 5, "comment": "Loved the chocolate flavor!"},
+        {"userName": "john", "rating": 4, "comment": "Great place, but always crowded."},
+        {"userName": "allison", "rating": 5, "comment": "Love the ice-creams and Scoopsie is super helpful!"}
+    ]
+}
+
+customizations = {
+    "options": [
+        {"customizationName": "Sugar-Free", "details": "Available for most flavors."},
+        {"customizationName": "Extra Toppings", "details": "Choose as many toppings as you want for an extra $5!"}
+    ]
+}
